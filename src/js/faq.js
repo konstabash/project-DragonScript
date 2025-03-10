11cf32c198f292c230bd2527d0ce6810cd74a332
Maksym Skubiak <skubiakcreates@gmail.com>
2025-03-10 19:52:53 +0100

    add acordeon

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,15 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const faqItems = document.querySelectorAll('.faq-item');
-
-  faqItems.forEach(item => {
-    item.addEventListener('click', function () {
-      this.classList.toggle('active');
-
-      faqItems.forEach(otherItem => {
-        if (otherItem !== this) {
-          otherItem.classList.remove('active');
-        }
-      });
-    });
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+document.addEventListener('DOMContentLoaded', () => {
+  new Accordion('.accordion-container', {
+    duration: 400,
+    showMultiple: false,
   });
 });
