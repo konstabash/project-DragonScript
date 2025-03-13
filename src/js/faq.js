ac838a0abf688d8380de3a757d0157125317b197
Konstantin <bashuk.kostya@gmail.com>
2025-03-13 12:39:12 +0200

    added multiple opened answers to faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -3,6 +3,6 @@ import 'accordion-js/dist/accordion.min.css';
 document.addEventListener('DOMContentLoaded', () => {
   new Accordion('.faq-accordion-container', {
     duration: 400,
-    showMultiple: false,
+    showMultiple: true,
   });
 });
